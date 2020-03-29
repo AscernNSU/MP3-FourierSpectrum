537d2474d6ba2eb6a7b03ea61842043eb300eb59
AscernNSU <alexey.romanov.nsu@gmail.com>
2020-03-29 18:15:55 +0700

    Everything is almost ready! Fixing last some bugs...

--- main.py
+++ main.py
@@ -1,17 +1,21 @@
-
 import os
-import librosa
+import numpy as np
+import matplotlib.pyplot as plt
 
-from flask import Flask, escape, request, flash, redirect, url_for, send_from_directory
+from flask import Flask, render_template, request, flash, redirect
+
 from werkzeug.utils import secure_filename
-#from librosa.core import load, stft
-
+from librosa import amplitude_to_db
+from librosa.display import specshow
+from librosa.core import load, stft
 
 UPLOAD_FOLDER = 'uploads/'
+STATIC_FOLDER = 'static/'
 ALLOWED_EXTENSIONS = {'mp3', 'wav'}
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates')
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['STATIC_FOLDER'] = STATIC_FOLDER
 
 
 def allowed_file(filename):
@@ -33,30 +37,62 @@ def upload_file():
             flash('No selected file')
             return redirect(request.url)
         if file and allowed_file(file.filename):
-            file_data = librosa.core.load(file.stream.read(), 44100)
             filename = secure_filename(file.filename)
             filename_ext = os.path.abspath(app.config['UPLOAD_FOLDER'] + filename)
-            #file.save(file.filename)
             file.save(filename_ext)
-            return redirect(url_for('uploaded_file',
-                                    filename=filename))
-    return '''
+            half_sampling_rate = 22050
+            stft_half_length = 1024
+            upload_folder_abspath = os.path.abspath(app.config['UPLOAD_FOLDER'])
+            upload_file_abspath = upload_folder_abspath + '\\' + filename
+            os.chmod(upload_file_abspath, 0o777)
+
+            file_data = load(upload_file_abspath, half_sampling_rate, True)
+            np_data = np.array(file_data[0])
+            plt.rcParams.update({'font.size': 12})
+            freq = np.linspace(0, 2 * half_sampling_rate, stft_half_length)
+            spec = stft(np_data, 2 * stft_half_length - 1)
+            abs_spec = np.abs(spec)
+
+            specshow(amplitude_to_db(abs_spec, ref=np.max), y_axis='log', x_axis='time')
+            plt.title('Спектрограмма мощности')
+            plt.colorbar(format='%+2.0f dB')
+            plt.tight_layout()
+
+            fig2d_path = os.path.abspath(app.config['STATIC_FOLDER'] + "spectrum-2D.png")
+            plt.savefig(fig2d_path)
+
+            fig2d_path = os.path.join(app.config['STATIC_FOLDER'], "spectrum-2D.png")
+            return render_template('mp3spec.html', fig_path=fig2d_path)
+
+    elif request.method == 'GET':
+        return '''
     <!doctype html>
-    <title>Upload new File</title>
-    <h1>Upload new File</h1>
-    <form method=post enctype=multipart/form-data>
-      <input type=file name=file>
-      <input type=submit value=Upload>
-    </form>
+    <title>Fourier spectrum visualization of audio mp3 File</title>
+    <h1 align="center" style="font-family:verdana">Добро пожаловать на страницу визуализации cпектра аудиофайлов!
+    Нажмите кнопку "Обзор...", чтобы загрузить аудиофайл. </h1>
+    <div align="center">
+        <form method=post enctype=multipart/form-data>
+          <input type=file name=file>
+          <input type=submit value=Upload>
+        </form>
+    </div>
+    '''
+    else:
+        return '''
+         <!doctype html>
+    <title>Fourier spectrum visualization of audio mp3 File</title>
+    <h1 align="center" style="font-family:verdana">Добро пожаловать на страницу визуализации спектра mp3!</h1>
     '''
 
 
-@app.route('/uploads/<filename>')
-def uploaded_file(filename):
-    upload_folder_abspath = os.path.abspath(app.config['UPLOAD_FOLDER'])
-    return send_from_directory(upload_folder_abspath,
-                               filename)
+@app.route('/templates/mp3spec.html', methods=['GET', 'POST'])
+def main_page():
+    if request.method == 'POST':
+        if request.form['main_page'] == 'Главная страница':
+            return redirect('/')
+    else:
+        return
 
 
-#if __name__ == '__main__':
-#    app.run()
+if __name__ == '__main__':
+    app.run()
